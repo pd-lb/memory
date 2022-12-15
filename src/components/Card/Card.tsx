814b4ac857745dc94516c33eef4a9f8a8b9f384b
pd-lb <120343588+pd-lb@users.noreply.github.com>
2022-12-15 16:13:43 +0100

    feat: implement card flipping

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Card.css'
-function Card() {
-  return <div className='card' />
+
+interface Props {
+  onClick: () => void
+  isFlipped: boolean
+}
+
+function Card(/*{ onClick, isFlipped }: Props*/) {
+  const [isFlipped, setIsFlipped] = useState(false)
+  return (
+    <div className={`card ${isFlipped ? 'flipped' : ''}`} onClick={() => setIsFlipped(!isFlipped)}>
+      <div className='card_inner'>
+        <div className='card_back' />
+        <div className='card_front'>&#128512;</div>
+      </div>
+    </div>
+  )
 }
 
 export default Card
