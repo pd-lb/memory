1ce4e6f4b6d8c354f2645d1f0353b0121dca1ed7
pd-lb <120343588+pd-lb@users.noreply.github.com>
2022-12-17 00:13:55 +0100

    feat: implement (re)starting the game from menu

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css'
 import PlayerInfo from './components/PlayerInfo/PlayerInfo'
 import Board from './components/Board/Board'
 import generateCards from './utils/generateCards'
+import Menu from './components/Menu/Menu'
 
 const App = () => {
   const cardPairs = 8
@@ -10,9 +11,16 @@ const App = () => {
   const [activePlayer, setActivePlayer] = useState(0)
   const [score, setScore] = useState([0, 0])
 
+  const resetGame = () => {
+    setCards(generateCards(cardPairs))
+    setActivePlayer(0)
+    setScore([0, 0])
+  }
+
   return (
     <div className='App'>
       <header className='App-header'>&#127356;&#127348;&#127356;&#127358;</header>
+      <Menu onNewGameClick={resetGame} />
       <PlayerInfo activePlayer={activePlayer} score={score} cardPairs={cardPairs} />
       <Board
         cards={cards}
---
+++ src/components/Menu/Menu.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './Menu.css'
+
+interface Props {
+  onNewGameClick: () => void
+}
+
+const Menu = ({ onNewGameClick }: Props) => {
+  const [open, setOpen] = useState(false)
+
+  return (
+    <div className={`menu ${open ? 'open' : ''}`}>
+      <div className='menu-toggle' onClick={() => setOpen(!open)}>
+        &#9776;
+      </div>
+      {open ? (
+        <div className='menu-content'>
+          <button onClick={onNewGameClick}>New Game</button>
+        </div>
+      ) : null}
+    </div>
+  )
+}
+
+export default Menu
