5641e4016370df1586401f89beb9b1bf026dbeb2
pd-lb <120343588+pd-lb@users.noreply.github.com>
2023-01-02 20:47:40 +0100

    refactor: extract card flip handler

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,4 +1,4 @@
-import React, { ComponentProps, useState } from 'react'
+import React, { ComponentProps, useCallback, useState } from 'react'
 import { Card } from '../Card/Card'
 import { useGetCardTransformations } from './utils/useGetCardTransformations'
 import './Board.css'
@@ -13,40 +13,60 @@ interface Props {
 
 const FLIP_BACK_DELAY_MS = 1500
 
+const handleCardFlip = (
+  card: ComponentProps<typeof Card>,
+  flippedCards: Props['cards'],
+  setFlippedCards: (flippedCards: Props['cards']) => void,
+  activePlayer: Props['activePlayer'],
+  setActivePlayer: Props['setActivePlayer'],
+  score: Props['score'],
+  setScore: Props['setScore']
+) => {
+  if (card.matchedBy != null || flippedCards.includes(card) || flippedCards.length > 1) {
+    return
+  }
+  setFlippedCards([...flippedCards, card])
+
+  if (flippedCards.length < 1) {
+    return
+  }
+
+  // there's a match
+  if (card.symbol === flippedCards[0].symbol) {
+    // indicate who matched the cards
+    card.matchedBy = activePlayer
+    flippedCards[0].matchedBy = activePlayer
+
+    setFlippedCards([])
+
+    // update active player's score
+    const scoreCopy = [...score]
+    scoreCopy[activePlayer] = ++scoreCopy[activePlayer]
+    setScore(scoreCopy)
+  } else {
+    setTimeout(() => {
+      setActivePlayer(Number(!activePlayer))
+      setFlippedCards([])
+    }, FLIP_BACK_DELAY_MS)
+  }
+}
+
 export const Board = ({ cards, activePlayer, setActivePlayer, score, setScore }: Props) => {
   const [flippedCards, setFlippedCards] = useState<Props['cards']>([])
   const cardTransformations = useGetCardTransformations(cards)
-
-  // TODO: try to extract this function
-  const handleCardFlip = (card: ComponentProps<typeof Card>) => {
-    if (card.matchedBy != null || flippedCards.includes(card) || flippedCards.length > 1) {
-      return
-    }
-    setFlippedCards([...flippedCards, card])
-
-    if (flippedCards.length < 1) {
-      return
-    }
-
-    // there's a match
-    if (card.symbol === flippedCards[0].symbol) {
-      // indicate who matched the cards
-      card.matchedBy = activePlayer
-      flippedCards[0].matchedBy = activePlayer
-
-      setFlippedCards([])
-
-      // update active player's score
-      const scoreCopy = [...score]
-      scoreCopy[activePlayer] = ++scoreCopy[activePlayer]
-      setScore(scoreCopy)
-    } else {
-      setTimeout(() => {
-        setActivePlayer(Number(!activePlayer))
-        setFlippedCards([])
-      }, FLIP_BACK_DELAY_MS)
-    }
-  }
+  const onCardClick = useCallback(
+    (card: ComponentProps<typeof Card>) =>
+      handleCardFlip(
+        card,
+        flippedCards,
+        setFlippedCards,
+        activePlayer,
+        setActivePlayer,
+        score,
+        setScore
+      ),
+    [flippedCards, activePlayer, setActivePlayer, score, setScore]
+  )
 
   return (
     <div className='board'>
@@ -57,7 +77,7 @@ export const Board = ({ cards, activePlayer, setActivePlayer, score, setScore }:
           <Card
             {...card}
             key={card.id}
-            onClick={() => handleCardFlip(card)}
+            onClick={() => onCardClick(card)}
             isFlipped={flippedCards.includes(card) || card.matchedBy != null}
             topOffset={top}
             leftOffset={left}
